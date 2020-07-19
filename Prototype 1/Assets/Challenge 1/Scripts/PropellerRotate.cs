de6a4a1bfd2ef517f3103e8cb67a9ca10eee5923
marienams <mhmsngh0@gmail.com>
2020-07-19 16:19:47 +0530

    Prototype 1 scenes from Create with Code (not complete)

---
+++ Prototype 1/Assets/Challenge 1/Scripts/PropellerRotate.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropellerRotate : MonoBehaviour
+{
+    //float degree = 90;
+    //Vector3 to = new Vector3(0, 0);
+    float angle = 90f;
+    public float rotationSpeed = 25.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, Time.deltaTime * rotationSpeed);
+        //transform.eulerAngles = Vector3.Lerp(transform.rotation.eulerAngles, to, Time.deltaTime);
+    }
+}
---
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 5.0f;                  //initialising your variable 
+    private float turnSpeed = 25f;
+    private float horizontalInput;
+
+    private float forwardInput;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+
+
+    void Update()
+    {
+        forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");  
+        //can understand input from a variety of gadgets like a mouse or controller
+
+        //we'll move our vehicle in this function
+        //transform.Translate(0,0,1);
+        transform.Translate(Vector3.forward * Time.deltaTime* speed* forwardInput);
+        //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed* horizontalInput); to shift left and right but with no rotation
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput); //shift left and right but with rotation
+        
+
+
+    }
+}
